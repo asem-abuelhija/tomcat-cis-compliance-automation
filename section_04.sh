17727c7440f42dfb13c45e19b14a080a6353d737
Asem Abuelhija <asem.adnan@gmail.com>
2022-02-22 15:19:44 +0200

    add headers to all sections

---
+++ section_04.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Tomcat CIS compliance Scan
+#
+# Author:
+#               Asem Abuelhija
+#
+#       22 - Feb - 2022
+#
+# All rights reserved (C) 2022
+#
+#################################################
